2201207f376a957ba6812258d9c10b59200ac118
Larry Gregory <lxg401@gmail.com>
2015-02-23 18:52:49 -0500

    Revert MKS API version to maintain compatibility with older versions of the MKS Server

--- src/main/java/hudson/scm/APISession.java
+++ src/main/java/hudson/scm/APISession.java
@@ -22,9 +22,9 @@ public class APISession
 	private static final Logger LOGGER = Logger.getLogger("IntegritySCM");
 	
 	// Store the API Version
-	public static final String VERSION = "4.13";
-	public static final int MAJOR_VERSION = 4;	
-	public static final int MINOR_VERSION = 13;
+    public static final String VERSION = "4.11";
+    public static final int MAJOR_VERSION = 4;
+    public static final int MINOR_VERSION = 11;
 	
 	// Class variables used to create an API Session
 	private String ipHostName;
